e034a24b833817830a8e8a961d238ff702d961e8
Dadicn <dzoniczbg99@gmail.com>
2021-10-05 10:20:20 +0200

    second-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'RegexValidation';
+   
+  loginForm = new FormGroup( {
+     email: new FormControl('', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
+     url: new FormControl('', [Validators.required, Validators.pattern("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})")]),
+     date: new FormControl('', [Validators.required, Validators.pattern("^(3[01]|[12][0-9]|0?[1-9])/(1[0-2]|0?[1-9])/(?:[0-9]{2})?[0-9]{2}$")])
+  })
+ 
+  get email(){return this.loginForm.get('email')}
+  get url(){return this.loginForm.get('url')}
+  get date(){return this.loginForm.get('date')}
+
+  get f()
+{
+    return this.loginForm.controls;
 }
+
+  onLogin() {
+    console.warn(this.loginForm.value);
+  }
+
+  }
+  
